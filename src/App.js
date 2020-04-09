d8923dbc133d8e05da683a9c9cd9a9c1cdc34f63
CoderGuy614 <jlutz.110@gmail.com>
2020-04-10 07:55:41 +0800

    Added logic to show / hide clear button

--- src/App.js
+++ src/App.js
@@ -26,13 +26,20 @@ class App extends Component {
     );
     this.setState({ users: res.data.items, loading: false });
   };
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar icon="fab fa-github" title="Github Finder" />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            showClear={this.state.users.length > 0 ? true : false}
+            clearUsers={this.clearUsers}
+            searchUsers={this.searchUsers}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
     text: "",
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -26,6 +33,15 @@ class Search extends Component {
           />
           <input type="submit" className="btn btn-dark btn-block" />
         </form>
+        {this.props.showClear && (
+          <button
+            onClick={this.props.clearUsers}
+            className="btn btn-light btn-block"
+          >
+            {" "}
+            Clear{" "}
+          </button>
+        )}
       </div>
     );
   }
