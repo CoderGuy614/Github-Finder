4a5d833d29000b83345cdc94dca603626ca761c6
CoderGuy614 <jlutz.110@gmail.com>
2020-04-11 19:13:51 +0800

    changed config and start scripts for heroku deployment

--- src/Context/github/GithubState.js
+++ src/Context/github/GithubState.js
@@ -10,6 +10,9 @@ import {
   GET_REPOS,
 } from "../types";
 
+const githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+const githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -24,7 +27,7 @@ const GithubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     dispatch({
@@ -36,7 +39,7 @@ const GithubState = (props) => {
   const searchUsers = async (text) => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: SEARCH_USERS,
@@ -47,7 +50,7 @@ const GithubState = (props) => {
   const getUser = async (username) => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({ type: GET_USER, payload: res.data });
   };
